6dff6d309fa1451959a594f57690222929da9e4a
Sushkov, Nikolai <Nikolai.Sushkov@billing.ru>
2018-02-19 20:12:12 +0300

    version 0.9

--- test/libraries/APIClients.py
+++ test/libraries/APIClients.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import requests
-import json
 import common_variables as com_vars
+from JsonValidator import *
+
 
 class APIClients:
 
@@ -10,8 +11,8 @@ class APIClients:
         body = {"client_id": client_id}
         body = json.dumps(body)
         r = requests.post(com_vars.BASE_API_URL+'client/services', headers=headers, data=body)
-        # services = json.loads(r.content)
         services = r.content
+        #services = json.loads(r.content)
         return services
 
     def get_services(self):
@@ -26,6 +27,63 @@ class APIClients:
         body = {"client_id": client_id, "service_id": service_id}
         body = json.dumps(body)
         r = requests.post(com_vars.BASE_API_URL + 'client/add_service', headers=headers, data=body)
-        print dir(r)
-
         return r.content
+
+    def get_available_services(self, client_id):
+        validator = JsonValidator()
+        all_serv = self.get_services()
+        cli_serv = self.get_client_services(client_id)
+        avail_serv_list = []
+        cl_serv_id_list = validator.get_elements(cli_serv, "$.items[*].id")
+        if not cl_serv_id_list:
+            cl_serv_id_list = []
+        all_id_list = validator.get_elements(all_serv, "$.items[*].id")
+
+
+        for id in cl_serv_id_list:
+            while id in all_id_list:
+                all_id_list.remove(id)
+
+        id_list = all_id_list
+        #id_list = [10, 22]
+
+        for id in id_list:
+            elem = validator.get_elements(all_serv, ("$.items[?(@.id == %s)]" %(id)))
+            if not elem:
+                continue
+            avail_serv_list.append(elem[0])
+
+        resp = {"count": len(avail_serv_list), "items": avail_serv_list}
+
+        return json.dumps(resp)
+
+    def check_service_activated(self, client_id, service_id):
+        validator = JsonValidator()
+        serv_list = self.get_client_services(client_id)
+        id_list = validator.get_elements(serv_list, "$.items[*].id")
+        if not id_list:
+            return False
+        if service_id in id_list:
+            return True
+        else:
+            return False
+
+
+id_list = [1, 22]
+value = 33
+while value in id_list:
+    id_list.remove(value)
+#print id_list
+
+
+
+
+
+client = APIClients()
+
+#print client.get_client_services(3)
+
+#print client.get_available_services(10)
+
+#print client.check_service_activated(3, 5)
+
