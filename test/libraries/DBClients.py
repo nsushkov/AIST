90d5a38238f83f603a660606341afdfe65c3da68
Sushkov, Nikolai <Nikolai.Sushkov@billing.ru>
2018-02-19 20:12:48 +0300

    version 0.9

--- test/libraries/DBClients.py
+++ test/libraries/DBClients.py
@@ -7,16 +7,20 @@ import common_variables as com_vars
 class DBClients:
 
     def get_client_balance(self, client_id):
-        with sqlite3.connect(com_vars.DB_NAME) as connection:
-            cursor = connection.cursor()
-            cursor.execute("SELECT BALANCE FROM BALANCES AS bl WHERE bl.CLIENTS_CLIENT_ID=%s;" %(client_id))
-            #cursor.execute("SELECT BALANCE FROM BALANCES AS bl WHERE bl.CLIENTS_CLIENT_ID={client_id}")
-            balance = cursor.fetchall()
-            if not balance:
-                balance = None
-            else:
-                balance = balance[0][0]
-        return balance
+        try:
+            with sqlite3.connect(com_vars.DB_NAME) as connection:
+                cursor = connection.cursor()
+                cursor.execute("SELECT BALANCE FROM BALANCES AS bl WHERE bl.CLIENTS_CLIENT_ID=%s;" %(client_id))
+                balance = cursor.fetchall()
+                if not balance:
+                    balance = None
+                else:
+                    balance = balance[0][0]
+                return balance
+        except sqlite3.DatabaseError as err:
+            print("Error: ", err)
+            return None
+
 
     def get_clients(self):
         with sqlite3.connect(com_vars.DB_NAME) as connection:
@@ -28,16 +32,20 @@ class DBClients:
         return clients
 
     def get_suitable_client(self):
-        with sqlite3.connect(com_vars.DB_NAME) as connection:
-            cursor = connection.cursor()
-            cursor.execute("select cl.CLIENT_ID from CLIENTS AS cl INNER JOIN BALANCES AS bl ON cl.CLIENT_ID = bl.CLIENTS_CLIENT_ID WHERE bl.BALANCE > 0 LIMIT 30;")
-            client_id_list = cursor.fetchall()
-            if not client_id_list:
-                client_id = None
-            else:
-                client_id = random.choice(client_id_list)[0]
-            print("CLIENT")
-        return client_id
+        try:
+            with sqlite3.connect(com_vars.DB_NAME) as connection:
+                cursor = connection.cursor()
+                cursor.execute("select cl.CLIENT_ID from CLIENTS AS cl INNER JOIN BALANCES AS bl ON cl.CLIENT_ID = bl.CLIENTS_CLIENT_ID WHERE bl.BALANCE > 0 LIMIT 30;")
+                client_id_list = cursor.fetchall()
+                if not client_id_list:
+                    client_id = None
+                else:
+                    client_id = random.choice(client_id_list)[0]
+                return client_id
+        except sqlite3.DatabaseError as err:
+            print("Error: ", err)
+            return None
+
 
     def test_select(self):
         with sqlite3.connect(com_vars.DB_NAME) as connection:
@@ -52,19 +60,17 @@ class DBClients:
 
     def add_rand_client(self):
         client_name = ''.join(random.choice(string.ascii_letters) for _ in range(com_vars.NAME_LEN))
-        with sqlite3.connect(com_vars.DB_NAME) as connection:
-            cursor = connection.cursor()
-            cursor.execute("INSERT INTO CLIENTS (CLIENT_NAME) VALUES ('%s')" %(client_name))
-            cursor.execute("INSERT INTO BALANCES VALUES (%s, %s)" %(cursor.lastrowid, com_vars.DEFAULT_BALANCE))
-            connection.commit()
-        return cursor.lastrowid
+        print "Создается новый клиент"
+        try:
+            with sqlite3.connect(com_vars.DB_NAME) as connection:
+                cursor = connection.cursor()
+                cursor.execute("INSERT INTO CLIENTS (CLIENT_NAME) VALUES ('%s')" %(client_name))
+                cursor.execute("INSERT INTO BALANCES VALUES (%s, %s)" %(cursor.lastrowid, com_vars.DEFAULT_BALANCE))
+                return cursor.lastrowid
+        except sqlite3.DatabaseError as err:
+            print("Error: ", err)
+            return None
 
-client = DBClients()
-print client.get_client_balance(11)
 
-with sqlite3.connect(com_vars.TEST_DB_PATH) as conn:
-    #cursor = connection.cursor()
-    #cursor.execute("select * from CLIENTS;")
-    #results = cursor.fetchall()
-    dirs = dir(conn)
+
 
