4b7d9a11d1f8f1c37becb8e96f6f275239399c1c
Sushkov, Nikolai <Nikolai.Sushkov@billing.ru>
2018-02-20 20:21:12 +0300

    release 1.0

--- test/libraries/common_variables.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-DB_NAME = "../app/web/clients.db"
-NAME_LEN = 6
-DEFAULT_BALANCE = 5
-BASE_API_URL = "http://localhost:5000/"
-
--- test/variables/common.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-DB_PATH = "../app/web/clients.db"
-NAME_LEN = 6
-DEFAULT_BALANCE = 5
-BASE_API_URL = "http://localhost:5000/"
